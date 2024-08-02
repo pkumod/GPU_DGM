c1ee9a7780ed66364877c1a200dadf4ea4a640db
Nick Lin <33555716+nicklin96@users.noreply.github.com>
2024-08-02 16:16:38 +0800

    add source

---
+++ cuSTINGER/include/main.hpp
@@ -0,0 +1,17 @@
+
+#ifndef _CU_MAIN_INCLUDE_H
+#define _CU_MAIN_INCLUDE_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+#include "utils.hpp"
+#include "update.hpp"
+#include "memoryManager.hpp"
+#include "cuStinger.hpp"
+
+#endif
+
